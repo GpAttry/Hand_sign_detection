bade8381c2baeb439a93f7c9295885b172dc3639
Gautam Priyadarshi Attry <itsgautam28@outlook.com>
2021-11-24 20:09:30 +0530

    Delete sign_detection.py

--- sign_detection.py
+++
@@ -1,5 +0,0 @@
-from keras.models import model_from_json
-from keras.preprocessing import image
-model = model_from_json(open("model1.json", "r").read())
-model.load_weights('weights.h5')
-print(model.summary())
